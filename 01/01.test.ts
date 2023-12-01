5f4b0187d5576361dcc16a6c370bc2d01919289f
Agustin Arenas <arenasagustin7@gmail.com>
2023-12-01 18:54:45 -0300

    Refactor tests for findFirstRepeated function

--- 01/01.test.ts
+++ 01/01.test.ts
@@ -4,14 +4,16 @@ import { expect, test } from "@jest/globals";
 const gifts = [2, 1, 3, 5, 3, 2];
 const gifts2 = [10, 20, 30];
 
-test("Test 1 Día 01", () => {
-  expect(typeof findFirstRepeated(gifts)).toBe("number");
-});
+describe("Pruebas para el día 01", () => {
+  test("Test 1: tiene que devolver un número", () => {
+    expect(typeof findFirstRepeated(gifts)).toEqual("number");
+  });
 
-test("Test 2 Día 01", () => {
-  expect(findFirstRepeated(gifts)).toBe(3);
-});
+  test("Test 2: tiene que devolver el primer número repetido", () => {
+    expect(findFirstRepeated(gifts)).toEqual(3);
+  });
 
-test("Test 3 Día 01", () => {
-  expect(findFirstRepeated(gifts2)).toBe(-1);
+  test("Test 3: tiene que devolver -1 en caso de no encontrar números repetidos", () => {
+    expect(findFirstRepeated(gifts2)).toEqual(-1);
+  });
 });
